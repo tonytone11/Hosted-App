d92f7f08db9e18ab2cc1f86d8380c344a4963b1f
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2024-12-15 15:47:05 -0500

    add my code-along code

---
+++ Hosted-Weather-Apps/public/js/app.js
@@ -0,0 +1,30 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+const temperatureOptions = document.querySelector('#temperature')
+
+messageOne.textContent = 'From JavaScript'
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    const temperature = temperatureOptions.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location + '&temperature=' + temperature).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+    console.log(location)
+})
